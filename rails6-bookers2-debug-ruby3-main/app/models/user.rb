30407f61bc1074b27cdaa9fec490dd01012405d6
zzriku <zzriku@users.noreply.github.com>
2023-11-02 07:26:29 +0000

    10/02/16:26

--- rails6-bookers2-debug-ruby3-main/app/models/user.rb
+++ rails6-bookers2-debug-ruby3-main/app/models/user.rb
@@ -7,7 +7,7 @@ class User < ApplicationRecord
   has_many :books
   has_one_attached :profile_image
 
-  validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
+  validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true, presence: true
   validates :introduction, length: {maximum: 50}
 
 
