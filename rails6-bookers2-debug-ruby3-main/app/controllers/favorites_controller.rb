a30e37878e02d8b86fd2a1cf06f48e8851b1a3fa
zzriku <zzriku@users.noreply.github.com>
2023-11-03 12:47:00 +0000

    10/03/favorite完了リアル

--- rails6-bookers2-debug-ruby3-main/app/controllers/favorites_controller.rb
+++ rails6-bookers2-debug-ruby3-main/app/controllers/favorites_controller.rb
@@ -1,15 +1,15 @@
 class FavoritesController < ApplicationController
   def create
-    book = Book.find(params[:book_id])
-    favorite = current_user.favorites.new(book_id: @book.id)
-    favprote.save
+    @book = Book.find(params[:book_id])
+    @favorite = current_user.favorites.new(book_id: @book.id)
+    @favorite.save
     redirect_to books_path
   end
 
   def destroy
-    book = Book.find(Prams[:book_id])
-    favorite = current_user.favorites.find_by(book_id: @book.id)
-    favorite.destroy
+    @book = Book.find(params[:book_id])
+    @favorite = current_user.favorites.find_by(book_id: @book.id)
+    @favorite.destroy
     redirect_to books_path
   end
 end
