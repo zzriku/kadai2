353c572fedb4f5201d444cc27a2aa3aecdec30d9
zzriku <zzriku@users.noreply.github.com>
2023-11-06 03:56:51 +0000

    11/06/12:56

--- rails6-bookers2-debug-ruby3-main/app/controllers/book_comments_controller.rb
+++ rails6-bookers2-debug-ruby3-main/app/controllers/book_comments_controller.rb
@@ -2,16 +2,19 @@ class BookCommentsController < ApplicationController
   def create
     @book = Book.find(params[:book_id])
     @book_comment = current_user.book_comments.new(book_comment_params)
-    @book_comment.save
     @book_comment.book_id = @book.id
-    redirect_to books_path
+    if @book_comment.save
+     redirect_to request.referer
+    else
+      Rails.logger.error @book_comment.errors.full_messages
+    end
   end
 
   def destroy
     @book = Book.find(params[:book_id])
     @book_comment = current_user.book_comments.find_by(book_id: @book.id)
     @book_comment.destroy
-    redirect_to books_path
+    redirect_to request.referer
   end
 
   private
