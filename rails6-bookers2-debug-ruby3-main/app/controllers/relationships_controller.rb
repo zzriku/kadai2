3c2b0e84a101a1e81e2672b2968488b749e2e0f3
zzriku <zzriku@users.noreply.github.com>
2023-11-06 03:25:24 +0000

    11/06

---
+++ rails6-bookers2-debug-ruby3-main/app/controllers/relationships_controller.rb
@@ -0,0 +1,25 @@
+class RelationshipsController < ApplicationController
+  def create
+    current_user.follow(params[:user_id])
+    redirect_to request.referer
+  end
+  def destroy
+    current_user.unfollow(params[:user_id])
+    redirect_to request.referer
+  end
+  
+  def followings
+    user = User.find(params[:user_id])
+    @users = user.followings
+  end
+
+  def followers
+    user = User.find(params[:user_id])
+    @users = user.followers
+  end
+  
+private
+  def relationship_params
+    params.require(:relationship).permit(:follower_id, :followed_id)
+  end
+end
